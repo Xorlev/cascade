c3f753d84ed6beb04d78c7d717c89e5906d8eeef
Michael Rose <elementation@gmail.com>
2019-02-25 19:33:25 -0700

    client: add reconnect timeout

--- slogd-client/src/lib.rs
+++ slogd-client/src/lib.rs
@@ -8,10 +8,15 @@ use std::sync::Arc;
 use grpcio::*;
 
 use slogd_shared::protos::rpc_grpc::StructuredLogClient;
+use std::time::Duration;
 
 pub fn new(host: &str) -> StructuredLogClient {
     let env = Arc::new(EnvBuilder::new().build());
-    let channel = ChannelBuilder::new(env).connect(host);
+    let channel = ChannelBuilder::new(env)
+        .initial_reconnect_backoff(Duration::from_millis(250))
+        .max_reconnect_backoff(Duration::from_millis(1000))
+        .keepalive_timeout(Duration::from_millis(1000))
+        .connect(host);
 
     StructuredLogClient::new(channel)
 }
